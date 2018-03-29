d2f0022e3f53737b5b7033895bf4e4243044727f
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2018-03-29 10:28:08 +0200

    Initialisation Repo
    + Application & customisation index.php (basé sur template Bootstrap)

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Projet de PHP">
+    <meta name="author" content="Julien SEIXAS & Romaine LOIRE">
+    <!-- <link rel="icon" href="../../../../favicon.ico"> -->
+
+    <title>Classement Overwatch</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.0/examples/cover/cover.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+          <h3>Classement</h3>
+      </header>
+
+      <main role="main" class="inner cover">
+
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>&copy; Julien SEIXAS & Romaine LOIRE - <?php print date("Y"); ?></p>
+        </div>
+      </footer>
+    </div>
+
+  </body>
+</html>
