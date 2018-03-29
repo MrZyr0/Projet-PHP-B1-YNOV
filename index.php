9162ca39caea65c6a398f04335118598fbdcbe9f
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2018-03-29 11:17:59 +0200

    Correction objet SQL
    + Suppression de la gestion singleton
    + Nettoyage est propriétés inutiles
    + Traduction des méthode en anglais
    + Ajout d'une methode exec pour effecter plusieurs requetes SQL d'une seule fois

--- index.php
+++ index.php
@@ -1,4 +1,36 @@
 <?php
+$RacineServ = __DIR__;
+require_once $RacineServ . '/src/SQL.php';
+
+$BDD = new SQL();
+$BDD->queryExec("
+                DROP TABLE IF EXISTS prismesport;
+                CREATE DATABASE prismesport;
+
+                CREATE TABLE equipe
+                (
+                  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                  `NomEquipe` varchar(255) DEFAULT NULL,
+                  `Victoires` varchar(255) DEFAULT 0,
+                  `Defaites` varchar(255) DEFAULT 0,
+                  PRIMARY KEY (`id`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+                INSERT INTO equipe (id, NomEquipe, Victoires, )
+                VALUES
+                	(1,'Boston Uprising'),
+                	(2,'Dallas Fuel'),
+                	(3,'Florida Mayhem'),
+                	(4,'Houston Outlaws'),
+                    (5,'London Spitfire');
+                    (6,'Los Angeles Gladiator');
+                    (7,'Los Angeles Valiant');
+                    (8,'New York Excelsior');
+                    (9,'Philadelphia Fusion');
+                    (10,'San Francisco Shock');
+                    (11,'Seoul Dynasty');
+                	(12,'Shanghai Dragons');
+            ");
 
 ?>
 
@@ -29,7 +61,7 @@
       </header>
 
       <main role="main" class="inner cover">
-
+          <pre style="color: white"></pre>
       </main>
 
       <footer class="mastfoot mt-auto">
---
+++ src/SQL.php
@@ -0,0 +1,33 @@
+<?php
+
+class SQL
+{
+    private $PDO = null;
+
+    function __construct()
+    {
+        $dsn = 'mysql:host=localhost';
+        $user = 'root';
+        $password = '';
+
+        $this->PDO = new PDO($dsn, $user, $password);
+    }
+
+    function queryStatement(string $Requette)
+    {
+        $Statement = $this->PDO->prepare($Requette);
+
+        $Statement->execute();
+
+        return $Statement->fetchAll();
+    }
+
+    function queryExec(string $Requette)
+    {
+        return $this->PDO->exec($Requette);
+    }
+
+
+}
+
+?>
