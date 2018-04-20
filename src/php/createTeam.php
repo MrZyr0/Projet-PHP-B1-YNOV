73831ab789d6c3d3d12f47a9f2d2d14bac2f3147
romainLoire <romain.loire@ynov.com>
2018-04-20 22:07:23 +0200

    createMatch
    ça marche bieng normalement

---
+++ src/php/createTeam.php
@@ -0,0 +1,54 @@
+<?php
+    session_start();
+    require_once $_SESSION["RacineServ"] . '/src/php/func/selectBackground.php';
+
+    $queryOK = false;
+    if (isset($_GET['createTeam']))
+    {
+        $teamName = $_POST['teamName'];
+        $BDD->queryCreateData("
+            INSERT INTO equipe (nomEquipe, victoires, defaites, nuls, goalAverage, image)
+            VALUES
+                    ('$teamName', '0', '0','0', '0',' ');
+");
+        $queryOK = true;
+    }
+?>
+
+<!doctype html>
+<html lang="fr" class="html">
+
+<?php require_once $_SESSION["RacineServ"] . '/src/php/templates/head.php'; ?>
+
+    <body class="page">
+
+        <header class="header">
+            <h3>Création d'équipe</h3>
+        </header>
+
+        <div class="body">
+            <?php
+                if ($queryOK == true)
+                {
+                    echo "<div class=\"alert alert-success\">";
+                    echo "<p>L'equipe  <strong> $teamName </strong>  a bien été crée.</p>";
+                    echo "</div>";
+                }
+            ?>
+            <form class="form" action="?createTeam" method="POST">
+
+                    <label>Nom equipe</label>
+                     <input type="text" name="teamName" />
+                <button type="submit" class="btn btn-primary my-1">Submit</button>
+            </form>
+
+            <nav class="menu">
+            <a href="/Projet-PHP/src/php/matchManager.php" class="btn">Gestion des Matchs</a>
+            <a href="/Projet-PHP/src/php/classement.php" class="btn">Classement</a>
+            </nav>
+    
+
+<?php require_once $_SESSION["RacineServ"] . '/src/php/templates/footer.php'; ?>
+
+    </body>
+</html>
